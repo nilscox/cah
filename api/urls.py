18828a3074d9e8354abe6f6f52af3b87468ba61f
Nils Layet <nils@epita.fr>
2018-01-16 01:02:37 +0100

    add debug views

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from api import views
+from api import views_debug
 
 urlpatterns = [
     path('player', views.PlayerViews.as_view()),
@@ -10,4 +11,9 @@ urlpatterns = [
     path('game/start', views.start_game),
     path('answer', views.answer),
     path('answer/select/<int:pk>', views.select),
+]
+
+urlpatterns += [
+    path('debug/ws_send/<nick>', views_debug.ws_send),
+    path('debug/ws_broadcast/<int:pk>', views_debug.ws_broadcast),
 ]
---
+++ api/views_debug.py
@@ -0,0 +1,40 @@
+import json
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from api.models import Player, Game
+
+
+@api_view(['POST'])
+def ws_send(request, nick):
+    data = json.loads(request.data.get('message'))
+
+    try:
+        player = Player.objects.get(nick=nick)
+    except Player.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if not player.socket_id:
+        return Response('Player is not connected', status=400)
+
+    print('[DEBUG]', 'ws send -> ' + player.nick + ': ' + str(data))
+    player.send(data)
+
+    return Response()
+
+
+@api_view(['POST'])
+def ws_broadcast(request, pk):
+    data = json.loads(request.data.get('message'))
+
+    try:
+        game = Game.objects.get(pk=pk)
+    except Game.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    print('[DEBUG]', 'ws broadcast -> ' + str(game) + ': ' + str(data))
+    game.broadcast(data)
+
+    return Response()
