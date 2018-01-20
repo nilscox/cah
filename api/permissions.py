3e70d19d88992406e9a07adb6d3d795489e8e4a0
Nils Layet <nils@epita.fr>
2018-01-20 02:55:45 +0100

    deny all http method for IsPlayer permissions

--- api/permissions.py
+++ api/permissions.py
@@ -6,7 +6,4 @@ from api.models import Player
 class IsPlayer(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
         return isinstance(request.user, Player)
