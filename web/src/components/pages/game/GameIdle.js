b6773e9dc9d1b1e0a1651f015cf672d57ebea348
Nils Layet <nils@epita.fr>
2018-02-17 01:27:34 +0100

    add scss for the game idle page

--- web/src/components/pages/game/GameIdle.js
+++ web/src/components/pages/game/GameIdle.js
@@ -12,25 +12,32 @@ const mapDispatchToProps = dispatch => ({
   onStart: () => dispatch(startGame()),
 });
 
+const StartButton = ({ onStart }) => (
+  <Button raised color="primary" className="start-button" onClick={onStart}>Start!</Button>
+);
+
 const GameIdle = ({ players, canStart, onStart }) => (
-  <div id="page-game" className="page">
-    <div className="game-idle">
+  <div className="page" id="page-game-idle">
+    <div className="container">
+
       <h2>Get ready!!</h2>
+
       <h4>Waiting for the game to start...</h4>
+
       {canStart && <StartButton onStart={onStart} />}
+
       <ul className="players-list">
         {players.map(player => (
-          <li key={player.nick} className={!player.connected ? 'player-offline' : null}>
+          <li
+            key={player.nick}
+            className={['player', !player.connected && 'player-offline'].toClassName()}>
             {player.nick}
           </li>
         ))}
       </ul>
+
     </div>
   </div>
-);
-
-const StartButton = ({ onStart }) => (
-  <Button raised color="primary" className="start-button" onClick={onStart}>Start!</Button>
 );
 
 export default connect(mapStateToProps, mapDispatchToProps)(GameIdle);
