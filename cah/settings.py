40bbe1d804822cfa0148f69fb8b7cfe2f3286d40
Nils Layet <nils@epita.fr>
2018-02-19 22:20:16 +0100

    add error handler middleware

--- cah/settings.py
+++ cah/settings.py
@@ -59,6 +59,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'api.middleware.ApiErrorMiddleware',
+    'api.middleware.SleepMiddleware',
 ]
 
 ROOT_URLCONF = 'cah.urls'
