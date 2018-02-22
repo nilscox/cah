e8a4d9fecba9322521a190f966b2e1b1073c2985
Nils Layet <nils@epita.fr>
2018-02-23 00:11:32 +0100

    add type for the player cards view component

--- web/src/components/pages/game/views/PlayerCardsView.js
+++ web/src/components/pages/game/views/PlayerCardsView.js
@@ -1,50 +1,70 @@
+// @flow
+
 import React from 'react';
 import { connect } from 'react-redux';
 
+import type { ChoiceType } from '../../../../types/models';
+import type {State} from '../../../../types/state';
+import { toClassName } from '../../../../utils';
 import { toggleChoice } from '../../../../actions/game';
 import ChoiceCard from '../../../common/ChoiceCard';
 
 const all = arr => arr.indexOf(false) < 0;
 
-const mapStateToProps = state => {
-  const { game, player, selection } = state;
-
-  return {
-    choices: state.player.cards,
-    isSelected: choice => selection.indexOf(choice) >= 0,
-    canSelectChoice: all([
-      game.state === 'started',
-      game.question_master !== player.nick,
-      !player.submitted,
-      selection.length < game.question.nb_choices,
-    ]),
-  };
-};
-
-const mapDispatchToProps = dispatch => ({
-  selectChoice: choiceId => dispatch(toggleChoice(choiceId)),
+const mapStateToProps = ({ game, player, selection }: State) => ({
+  choices: player.cards,
+  isSelected: choice => selection.indexOf(choice) >= 0,
+  canSelectChoice: all([
+    game.state === 'started',
+    game.question_master !== player.nick,
+    !player.submitted,
+    selection.length < game.question.nb_choices,
+  ]),
 });
 
-const PlayerCardsView = ({ choices, isSelected, canSelectChoice, selectChoice }) => (
-  <div className="game-view" id="player-cards">
+const mapDispatchToProps = dispatch => ({
+  toggleChoice: choice => dispatch(toggleChoice(choice.id)),
+});
 
-    <div className="cards-list">
+type PlayerCardsViewProps = {
+  choices: Array<ChoiceType>,
+  isSelected: ChoiceType => boolean,
+  canSelectChoice: boolean,
+  toggleChoice: ChoiceType => void,
+};
 
-      {choices && choices.map(choice => (
-        <ChoiceCard
-          key={choice.id}
-          choice={choice}
-          className={[
-            isSelected(choice) && 'selected',
-            canSelectChoice && 'can-select',
-          ].toClassName()}
-          onClick={() => (canSelectChoice || isSelected(choice)) && selectChoice(choice)}
-        />
-      ))}
+const PlayerCardsView = ({
+  choices,
+  isSelected,
+  canSelectChoice,
+  toggleChoice,
+}: PlayerCardsViewProps) => {
+  const onCardClicked = choice => {
+    if (canSelectChoice || isSelected(choice))
+      toggleChoice(choice);
+  }
+
+  return (
+    <div className="game-view" id="player-cards">
+
+      <div className="cards-list">
+
+        {choices && choices.map(choice => (
+          <ChoiceCard
+            key={choice.id}
+            choice={choice}
+            className={toClassName([
+              isSelected(choice) && 'selected',
+              canSelectChoice && 'can-select',
+            ])}
+            onClick={() => onCardClicked(choice)}
+          />
+        ))}
+
+      </div>
 
     </div>
-
-  </div>
-);
+  );
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(PlayerCardsView);
