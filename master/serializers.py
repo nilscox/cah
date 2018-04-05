a538847694b7dbbc94b6a7d62a8c6b824b81df8f
Nils Layet <nils@epita.fr>
2018-04-05 15:13:03 +0200

    handle json array for master data question blanks field

--- master/serializers.py
+++ master/serializers.py
@@ -1,8 +1,26 @@
+import json
 from rest_framework import serializers
 from master.models import Question, Choice
 
+class JSONStrField(serializers.Field):
+
+    def to_representation(self, obj):
+        return json.loads(obj)
+
+    def to_internal_value(self, data):
+        if type(data) is not list:
+            raise serializers.ValidationError('blanks must be an array')
+
+        for blank in data:
+            if type(blank) is not int:
+                raise serializers.ValidationError('blanks must be an array of integers')
+
+        return json.dumps(data)
+
 
 class QuestionSerializer(serializers.ModelSerializer):
+    blanks = JSONStrField(default=[])
+
     class Meta:
         model = Question
         fields = ('id', 'text', 'blanks')
