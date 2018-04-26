da2f1c5179b25c60c64520780aa81bb31d586fd0
Nils Layet <nils@epita.fr>
2018-04-26 23:31:43 +0200

    disable wierd lint error about not finding Svg in expo

--- app/src/lobby/LobbyScreen.js
+++ app/src/lobby/LobbyScreen.js
@@ -3,6 +3,7 @@
 import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { connect } from 'react-redux';
+/* eslint-disable-next-line import/named */
 import { Svg } from 'expo';
 
 import type { Game } from '~/types/game';
