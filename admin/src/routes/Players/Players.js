9aff145cddf7dc328b8ef98eb7b3ced2f1a68fb0
Bopzor <violaine.ballandin@gmail.com>
2018-06-17 15:20:56 +0200

    start Players List integration. use json file for data

--- admin/src/routes/Players/Players.js
+++ admin/src/routes/Players/Players.js
@@ -1,7 +1,38 @@
 import * as React from 'react';
+import { Table } from 'react-bootstrap';
 
-const Players = () => (
-  <h1>Players</h1>
+const playersList = require('../../playersList');
+
+const Row = ({ player }) => (
+    <tr>
+        <td>{player.nick}</td>
+        <td>{player.connected.toString()}</td>
+        <td>{player.score}</td>
+    </tr>
 );
 
-export default Players;
+const Players = ({ players }) => (
+    <div>
+
+        <h1>Players</h1>
+
+        <Table bordered condensed striped>
+
+            <thead>
+                <tr>
+                    <th>Nick</th>
+                    <th>Connected</th>
+                    <th>Score</th>
+                </tr>    
+            </thead>
+
+            <tbody>
+                {players.map((player) => <Row key={`player-${player.nick}`} player={player} />)}
+            </tbody>
+
+        </Table>
+
+    </div>
+);
+
+export default () => <Players players={playersList} />;
