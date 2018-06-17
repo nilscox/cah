b890cb6a2182c97e18d46dfc925bf45ab8045c26
Nils Layet <nils@epita.fr>
2018-06-17 22:19:06 +0200

    reduce game history

--- admin/src/redux/actions/games.js
+++ admin/src/redux/actions/games.js
@@ -4,13 +4,16 @@ export const listGames = () => ({
   route: `/api/admin/game/`,
 });
 
-export const GAME_FETCH_HISTORY = 'GAMES_FETCH_HISTORIES';
-export const fetchGameHistory = (game) => ({
-  type: GAME_FETCH_HISTORY,
-  route: `/api/admin/game/${game.id}/history/`,
-});
-
 export const GAMES_FETCH_HISTORIES = 'GAMES_FETCH_HISTORIES';
 export const fetchGamesHistories = (games) => (dispatch) => {
   return Promise.all(games.map(game => dispatch(fetchGameHistory(game))));
 };
+
+export const GAME_FETCH_HISTORY = 'GAME_FETCH_HISTORY';
+export const fetchGameHistory = (game) => ({
+  type: GAME_FETCH_HISTORY,
+  route: `/api/admin/game/${game.id}/history/`,
+  meta: {
+    gameId: game.id,
+  },
+});
--- admin/src/redux/reducers/games.js
+++ admin/src/redux/reducers/games.js
@@ -1,15 +1,45 @@
 import { handle } from 'redux-pack';
 
-import { GAMES_LIST } from '../actions';
+import { GAMES_LIST, GAME_FETCH_HISTORY } from '../actions';
 
 export default (state = [], action) => {
-  const { type, payload } = action;
+  const { type, payload, meta } = action;
 
   const handlers = {
     [GAMES_LIST]: {
       start   : () => [],
       success : () => payload.map(game => ({ ...game, turns: [] })),
       failure : () => [],
+    },
+    [GAME_FETCH_HISTORY]: {
+      start   : (games) => {
+        const { gameId } = meta;
+
+        const idx = games.findIndex(game => game.id === gameId);
+
+        if (idx < 0)
+          return games;
+
+        return [
+          ...games.slice(0, idx),
+          { ...games[idx], turns: [] },
+          ...games.slice(idx + 1),
+        ];
+      },
+      success : (games) => {
+        const { gameId } = meta;
+
+        const idx = games.findIndex(game => game.id === gameId);
+
+        if (idx < 0)
+          return games;
+
+        return [
+          ...games.slice(0, idx),
+          { ...games[idx], turns: payload },
+          ...games.slice(idx + 1),
+        ];
+      },
     },
   };
 
