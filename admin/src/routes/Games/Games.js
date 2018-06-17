181a60694bb8c2e3c1e56ace054c9b7c49b3e346
Bopzor <violaine.ballandin@gmail.com>
2018-06-17 15:02:19 +0200

    start Games List integration. use json file for data

--- admin/src/routes/Games/Games.js
+++ admin/src/routes/Games/Games.js
@@ -1,7 +1,44 @@
 import * as React from 'react';
+import { Table } from 'react-bootstrap';
 
-const Games = () => (
-  <h1>Games</h1>
+const gamesList = require('../../gamesList');
+
+const Row = ({ game }) => (
+    <tr>
+        <td>{game.id}</td>
+        <td>{game.owner}</td>
+        <td>{game.players.length}</td>
+        <td>{game.state}</td>
+        <td>{game.play_state && game.play_state.replace(/_/g, ' ')}</td>
+        <td>{game.turns.length}</td>
+    </tr>
 );
 
-export default Games;
+const Games = ({ games }) => (
+  <div>
+
+      <h1>ADD GAME</h1>
+
+      <Table bordered condensed striped>
+
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>Owner</th>
+                <th>Players</th>
+                <th>State</th>
+                <th>Play State</th>
+                <th>Turn</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            {games.map(game => <Row  key={`game-${game.id}`} game={game} />)}
+        </tbody>
+
+      </Table>
+
+  </div>
+);
+
+export default () => <Games games={gamesList} />;
