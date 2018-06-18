6ebcf3d0ed817dc14d707c4b4a949d51d0141283
Bopzor <violaine.ballandin@gmail.com>
2018-06-18 22:52:54 +0200

    GamesList is now a component

--- admin/src/routes/Games/Games.js
+++ admin/src/routes/Games/Games.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { Table } from 'react-bootstrap';
 
-import CreateGame from './CreateGame';
 import GamesInfo from './GamesInfo';
+import CreateGame from './CreateGame';
+import GamesList from './GamesList';
 
 import './Games.css';
 
@@ -13,17 +13,6 @@ const mapStateToProps = (state) => {
     players: state.get('players').toJSON(),
   };
 };
-
-const Row = ({ game }) => (
-  <tr>
-    <td>{game.id}</td>
-    <td>{game.owner}</td>
-    <td>{game.players.length}</td>
-    <td>{game.state}</td>
-    <td>{game.play_state && game.play_state.replace(/_/g, ' ')}</td>
-    <td>{game.turns.length}</td>
-  </tr>
-);
 
 const Games = ({ games, players }) => (
   <div className="games">
@@ -36,24 +25,7 @@ const Games = ({ games, players }) => (
 
     </div>
 
-    <Table className="games-list" bordered condensed striped>
-
-      <thead>
-        <tr>
-          <th>id</th>
-          <th>Owner</th>
-          <th>Players</th>
-          <th>State</th>
-          <th>Play State</th>
-          <th>Turn</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        {games.map((game) => <Row  key={`game-${game.id}`} game={game} />)}
-      </tbody>
-
-    </Table>
+    <GamesList games={games} />
 
   </div>
 );
