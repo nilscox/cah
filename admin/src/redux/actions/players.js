b2be888c39793576c60ab606befd256a30f1c279
Bopzor <violaine.ballandin@gmail.com>
2018-06-19 23:34:02 +0200

    add createPlayer action

--- admin/src/redux/actions/players.js
+++ admin/src/redux/actions/players.js
@@ -3,3 +3,13 @@ export const listPlayers = () => ({
   type: PLAYERS_LIST,
   route: `/api/admin/player/`,
 });
+
+export const PLAYER_CREATE = 'PLAYER_CREATE';
+export const createPlayer = (nick) => ({
+  type: PLAYER_CREATE,
+  route: `/api/admin/player/`,
+  method: `POST`,
+  body: {
+    nick: nick,
+  },
+});
