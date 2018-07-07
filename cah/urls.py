bbe1e489bb95aa113b35e4167e9e4f686474ab9a
Nils Layet <nils@epita.fr>
2018-07-07 13:30:58 +0200

    add static files to the api

--- cah/urls.py
+++ cah/urls.py
@@ -1,6 +1,8 @@
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^api/', include('api.urls')),
     url(r'^api/master/', include('master.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
