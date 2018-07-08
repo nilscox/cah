dc01420f5cf782caa1674596f801e5bf2d2e2d02
Nils Layet <nils@epita.fr>
2018-07-08 21:24:33 +0200

    fix games reducer

--- admin/src/redux/reducers/games.js
+++ admin/src/redux/reducers/games.js
@@ -10,40 +10,40 @@ import {
 
 const GAME_CREATED = 'GAME_CREATED';
 const GAME_STARTED = 'GAME_STARTED';
-const PLAYER_JOINED = 'PLAYER_JOINED';
-const PLAYER_LEFT = 'PLAYER_LEFT';
-const ANSWER_SUBMITTED = 'ANSWER_SUBMITTED';
-const ANSWER_SELECTED = 'ANSWER_SELECTED';
+const GAME_PLAYER_JOINED = 'GAME_PLAYER_JOINED';
+const GAME_PLAYER_LEFT = 'GAME_PLAYER_LEFT';
+const GAME_ANSWER_SUBMITTED = 'GAME_ANSWER_SUBMITTED';
+const GAME_ANSWER_SELECTED = 'GAME_ANSWER_SELECTED';
 const GAME_NEXT_TURN = 'GAME_NEXT_TURN';
 
 // eslint-disable-next-line eqeqeq
 const findGameIdx = (games, gameId) => games.findIndex(game => game.id == gameId);
 
-const findPlayerIdx = (players, nick) => players.findIndex(player => player.nick === nick);
-
 const websocket = (state, message) => {
-  const gameIdx = message.game && findGameIdx(state, message.game.id);
+  const gameIdx = message.gameId && findGameIdx(state, message.gameId);
 
   switch (message.type) {
-  case PLAYER_JOINED:
+  case GAME_PLAYER_JOINED:
     return state.merge([gameIdx, 'players'], [message.player]);
 
-  case PLAYER_LEFT:
+  case GAME_PLAYER_LEFT:
     const game = state[gameIdx];
     const players = game.players;
 
-    return state.merge([gameIdx, 'players'], players.splice(findPlayerIdx(players, message.player.nick), 1));
+    return state.set([gameIdx, 'players'], players.filter(p => p.nick !== message.nick));
 
   case GAME_CREATED:
     return state.push(message.game);
 
   case GAME_STARTED:
   case GAME_NEXT_TURN:
-  case ANSWER_SUBMITTED:
     return state.set(gameIdx, message.game);
 
-  case ANSWER_SELECTED:
-    return state.merge([gameIdx, 'turns'], message.turn);
+  case GAME_ANSWER_SUBMITTED:
+    return state.merge([gameIdx, 'propositions'], [message.answer])
+
+  case GAME_ANSWER_SELECTED:
+    return state.merge([gameIdx, 'turns'], [message.turn]);
 
   default:
     return state;
