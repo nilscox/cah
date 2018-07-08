25a990ab158da2d4c6a263e1b7f6d5f8e559c7ad
Nils Layet <nils@epita.fr>
2018-07-08 21:30:17 +0200

    refactor test

--- admin/test/reducers/games.test.js
+++ admin/test/reducers/games.test.js
@@ -3,23 +3,25 @@ import crio from 'crio';
 
 import reducer from '../../src/redux/reducers/games';
 
+const testWS = (message, state, expected) => {
+  const action = {
+    type: 'WS_MESSAGE',
+    message,
+  };
+
+  return expect(reducer(crio(state), action)).to.deep.equal(crio(expected));
+}
+
 describe('games reducers', () => {
 
-  it('should process a WS_GAME_PLAYER_JOINED action', () => {
-    const state = crio([{ id: 42, players: [] }]);
-    const action = {
-      type: 'WS_MESSAGE',
-      message: {
-        type: 'GAME_PLAYER_JOINED',
-        gameId: 42,
-        player: { nick: 'toto' },
-      },
-    };
-    const expected = [
-      { id: 42, players: [{ nick: 'toto' }] },
-    ];
-
-    expect(reducer(state, action)).to.deep.equal(crio(expected));
-  });
+  it('should process a WS_GAME_PLAYER_JOINED action', () => testWS(
+    {
+      type: 'GAME_PLAYER_JOINED',
+      gameId: 42,
+      player: { nick: 'toto' },
+    },
+    [{ id: 42, players: [] }],
+    [{ id: 42, players: [{ nick: 'toto' }] }],
+  ));
 
 });
