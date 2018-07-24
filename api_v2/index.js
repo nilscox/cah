74301ebe528fc709cd726ae9d442a7735c877978
Nils Layet <nils@epita.fr>
2018-07-24 16:26:30 +0200

    extract app from index.js

--- api_v2/index.js
+++ api_v2/index.js
@@ -1,59 +1,6 @@
-global.Promise = require('bluebird');
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-
-const { Player } = require('./models');
-const routes = require('./routes');
-const { APIError } = require('./errors');
+const app = require('./app');
 
 const PORT = process.env.CAH_API_PORT || 4242;
-
-const app = express();
-
-app.set('x-powered-by', false);
-
-app.use(bodyParser.json());
-
-app.use(session({
-  secret: 'kernelpanic',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-}));
-
-app.use((req, res, next) => {
-  res.append('Access-Control-Allow-Origin', '*');
-  res.append('Access-Control-Allow-Headers', 'Authorization');
-  next();
-});
-
-app.use((req, res, next) => {
-  if (!req.session.player)
-    return next();
-
-  Player.find({ where: { nick: req.session.player }})
-    .then(player => {
-      if (!player) {
-        delete req.session.player;
-        next();
-      }
-
-      req.player = player;
-      next();
-    })
-    .catch(next);
-});
-
-app.use('/api', routes);
-
-app.use((err, req, res, next) => {
-  if (!(err instanceof APIError))
-    throw err;
-
-  res.status(err.status).json(err.toJSON());
-});
 
 console.log('starting server on port ' + PORT);
 app.listen(PORT);
