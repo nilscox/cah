22f3b8cd894582cd6881f7cffe39b028750d1174
Nils Layet <nils@epita.fr>
2018-07-28 16:01:16 +0200

    add player creation test

--- api_v2/test/player.test.js
+++ api_v2/test/player.test.js
@@ -3,31 +3,53 @@ const expect = require('chai').expect;
 
 describe('player', () => {
 
-  describe('player crud', () => {
+  describe('crud', () => {
 
-    it('should list all players 0', function() {
-      return request(this.app)
-        .get('/api/player/list')
-        .expect(200)
-        .then(res => {
-          expect(res.body).to.be.a('array');
-          expect(res.body).to.have.length(0);
-        });
+    describe('list', () => {
+
+      it('should list all players 0', function() {
+        return request(this.app)
+          .get('/api/player/list')
+          .expect(200)
+          .then(res => {
+            expect(res.body).to.be.a('array');
+            expect(res.body).to.have.length(0);
+          });
+      });
+
+      it('should list all players 2', async function() {
+        const { Player } = this.models;
+
+        await new Player({ nick: 'nils' }).save();
+        await new Player({ nick: 'tom' }).save();
+
+        return request(this.app)
+          .get('/api/player/list')
+          .expect(200)
+          .then(res => {
+            expect(res.body).to.be.a('array');
+            expect(res.body).to.have.length(2);
+          });
+      });
+
     });
 
-    it('should list all players 2', async function() {
-      const { Player } = this.models;
+    describe('create', () => {
 
-      await new Player({ nick: 'nils' }).save();
-      await new Player({ nick: 'tom' }).save();
+      it('should create a new player', function() {
+        return request(this.app)
+          .post('/api/player')
+          .send({ nick: 'nils' })
+          .expect(201)
+          .then(res => {
+            expect(res.body).to.exist;
+            expect(res.body).to.have.property('id');
+            expect(res.body).to.have.property('nick', 'nils');
+            expect(res.body).to.have.property('avatar', null);
+          });
 
-      return request(this.app)
-        .get('/api/player/list')
-        .expect(200)
-        .then(res => {
-          expect(res.body).to.be.a('array');
-          expect(res.body).to.have.length(2);
-        });
+      });
+
     });
 
   });
