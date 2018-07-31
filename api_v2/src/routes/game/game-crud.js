56cf02f85967e38a01f96dd1db58dd4664df05d8
Nils Cox <nils@epita.fr>
2018-07-31 23:05:46 +0200

    add game owner when creating the game

--- api_v2/src/routes/game/game-crud.js
+++ api_v2/src/routes/game/game-crud.js
@@ -18,9 +18,8 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res, next) => {
   GameValidator.validate(req.body)
-    .then(game => Game.create(game))
+    .then(game => Game.create({ ...game, ownerId: req.player.id }))
     .tap(game => game.addPlayer(req.player))
-    .tap(game => game.setOwner(req.player))
     .tap(game => game.reload({ include: ['players', 'owner'] }))
     .then(game => res.status(201).format(GameFormatter, game))
     .catch(next);
