078386eeaa304835547a0e1d16edefbe322705b4
Nils Cox <nils@epita.fr>
2018-08-07 23:58:09 +0200

    rm api v1

--- api/events/events_processor.py
+++
@@ -1,7 +0,0 @@
-class EventProcessor:
-
-    def process(self, event, *args, **kwargs):
-        method = getattr(self, "on_" + event, None)
-
-        if method:
-            method(*args, **kwargs)
--- master/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from master import views
-
-router = DefaultRouter()
-
-router.register(r'questions', views.QuestionViewSet, base_name='question')
-router.register(r'choices', views.ChoiceViewSet, base_name='choice')
-
-urlpatterns = router.urls
