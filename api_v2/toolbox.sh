a7444ae27a0ecc183bef8064df0c70e248fdec04
Nils Cox <nils@epita.fr>
2018-08-07 13:41:28 +0200

    use functions only in toolbox.sh

--- api_v2/toolbox.sh
+++ api_v2/toolbox.sh
@@ -2,24 +2,33 @@
 
 export HTTP_SESSION="default"
 
-function session { export HTTP_SESSION="$1"; login "nick=${HTTP_SESSION}" }
+function session {
+  if [ -z "$1" ]; then
+    echo "usage: $0 <name>"
+    return 1
+  fi
+
+  export HTTP_SESSION="$1"
+  login "$1"
+}
+
 function h() { http --session "/tmp/httpie.${HTTP_SESSION}.session" -v $@ }
 
-alias players='h :4242/api/player'
-alias games='h :4242/api/game'
+function players() { h ":4242/api/player" }
 function player() { h ":4242/api/player/$1" }
+function games() { h ":4242/api/game" }
 function game() { h ":4242/api/game/$1" }
 
-alias login='h POST :4242/api/player/login'
-alias logout='h POST :4242/api/player/logout'
+function create_player() { h POST ":4242/api/player" nick="$1" }
+function create_game() { h POST ":4242/api/game" lang="$1" }
 
-alias me='h :4242/api/player/me'
+function login() { h POST ":4242/api/player/login" nick="$1" }
+function logout() { h POST ":4242/api/player/logout" }
 
-alias create_player='h POST :4242/api/player'
-alias create_game='h POST :4242/api/game'
+function me() { h ":4242/api/player/me" }
 
 function game_join() { h POST ":4242/api/game/$1/join" }
 function game_start() { h POST ":4242/api/game/$1/start" }
-function game_answer() { h POST ":4242/api/game/$1/answer" }
-function game_select() { h POST ":4242/api/game/$1/select" }
+function game_answer() { h POST ":4242/api/game/$1/answer" ids:="$2" }
+function game_select() { h POST ":4242/api/game/$1/select" id:="$2" }
 function game_next() { h POST ":4242/api/game/$1/next" }
