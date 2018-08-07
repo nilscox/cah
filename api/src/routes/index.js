5979ac1821121454d01865a02703712314ff706b
Nils Cox <nils@epita.fr>
2018-08-08 00:26:57 +0200

    v0.1.0

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
+const pkg = require('../../package');
+
 const player = require('./player');
 const game = require('./game');
 
 const router = express.Router();
 
+router.use('/version', (req, res) => res.send(pkg.version));
 router.use('/player', player);
 router.use('/game', game);
 
