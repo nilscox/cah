8f812c19a80688a13a47de5963712ab65f3ce56a
Nils Cox <nils@epita.fr>
2018-08-08 20:09:35 +0200

    api: support admin for the game history route

--- api/src/routes/game/game-history.js
+++ api/src/routes/game/game-history.js
@@ -1,6 +1,6 @@
 const { Game } = require('../../models');
 const { gameTurnFormatter } = require('../../formatters');
-const { isPlayer, isInGame } = require('../../permissions');
+const { isAdmin, isPlayer, isInGame } = require('../../permissions');
 const findGame = require('./find-game');
 
 const router = require('../createRouter')();
@@ -9,10 +9,15 @@ module.exports = router.router;
 router.param('id', findGame);
 
 router.get('/:id/history', {
-  authorize: [
-    req => isPlayer(req.player),
-    req => isInGame(req.player, req.params.id),
-  ],
+  authorize: {
+    or: [
+      req => isAdmin(req.admin),
+      [
+        req => isPlayer(req.player),
+        req => isInGame(req.player, req.params.id),
+      ],
+    ],
+  },
   format: gt => gameTurnFormatter.full(gt, { many: true }),
 }, async (req, res, next) => {
   return await req.params.game.getTurns({
