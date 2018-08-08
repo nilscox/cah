f35a16f3b68cd65dabd22b40e2025eceed4926d8
Nils Cox <nils@epita.fr>
2018-08-08 10:27:49 +0200

    api: support object in authorization

--- api/src/routes/createRouter.js
+++ api/src/routes/createRouter.js
@@ -1,17 +1,56 @@
 const express = require('express');
 const authorize = require('../authorize');
+const { AuthenticationError } = require('../errors');
 
 const authorizeRequest = async (req, authorize) => {
   if (!authorize)
     return;
 
   const authorize_function = (req, f) => Promise.resolve(f(req));
-  const authorize_array = (req, arr) => Promise.mapSeries(arr, a => authorizeRequest(req, a));
+  const authorize_and = (req, arr) => Promise.mapSeries(arr, a => authorizeRequest(req, a));
+  const authorize_or = async (req, arr) => {
+    let error = null;
+
+    for (let i = 0; i < arr.length; ++i) {
+      try {
+        return await authorizeRequest(req, arr[i]);
+      } catch (e) {
+        if (!(e instanceof AuthenticationError))
+          throw e;
+
+        error = e;
+      }
+    }
+
+    if (error)
+      throw error;
+  };
+
+  const authorize_object = (req, obj) => {
+    const keys = Object.keys(obj);
+
+    for (let i = 0; i < keys.length; ++i) {
+      const op = keys[i];
+
+      switch (op) {
+      case 'or':
+        return authorize_or(req, obj[op]);
+
+      case 'and':
+        return authorize_and(req, obj[op]);
+
+      default:
+        throw new Error('invalid operator ' + op);
+      }
+    }
+  };
 
   if (typeof authorize === 'function')
     await authorize_function(req, authorize);
   else if (authorize instanceof Array)
-    await authorize_array(req, authorize);
+    await authorize_and(req, authorize);
+  else if (typeof authorize === 'object')
+    await authorize_object(req, authorize);
   else
     throw new Error('invalid authorizer ' + typeof authorize);
 }
