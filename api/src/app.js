567803a08a441c9d2ac5af858dfb4f1235df2ee9
Nils Cox <nils@epita.fr>
2018-08-08 19:33:06 +0200

    api: prevent from launching the api without an admin token

--- api/src/app.js
+++ api/src/app.js
@@ -12,6 +12,11 @@ const { APIError } = require('./errors');
 
 const ADMIN_TOKEN = process.env.CAH_API_ADMIN_TOKEN;
 
+if (!ADMIN_TOKEN) {
+  console.log('missing env: CAH_API_ADMIN_TOKEN');
+  process.exit(1);
+}
+
 const app = express();
 
 app.set('x-powered-by', false);
