53f62b6612ae0a3a3baae11b6b6bb73ffff39e7d
Nils Cox <nils@epita.fr>
2018-08-11 15:02:06 +0200

    api: change admin authentication process using session instead

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -5,10 +5,20 @@ const pkg = require('../../package');
 const player = require('./player');
 const game = require('./game');
 
+const API_ADMIN_TOKEN = process.env.CAH_API_ADMIN_TOKEN;
+
 const router = express.Router();
 
 router.use('/version', (req, res) => res.send(pkg.version));
 router.use('/player', player);
 router.use('/game', game);
+
+router.post('/admin', (req, res) => {
+  if (req.body.token !== API_ADMIN_TOKEN)
+    return res.status(401).end();
+
+  req.session.admin = true;
+  res.end();
+})
 
 module.exports = router;
