813ab9ce9e9eb6bfaddbb3ead931ff8977ccbb60
Nils Cox <nils@epita.fr>
2018-08-11 15:55:02 +0200

    api: refactor utils (getEnv)

--- api/src/utils.js
+++ api/src/utils.js
@@ -1,18 +1,33 @@
 const fs = require('fs');
 const path = require('path');
 
+const getEnv = (key, defaultValue) => {
+  const value = process.env[key];
+
+  if (value)
+    return value;
+
+  if (defaultValue !== undefined)
+    return defaultValue;
+
+  try { throw new Error('missing env: ' + key) }
+  catch (e) { log('FATAL', 'ENV', e) }
+  finally { process.exit(1) }
+};
+
 const log = (level, tag, ...message) => {
   const match = new Date().toISOString().match(/^(\d{4}-\d{2}-\d{2})T(\d{2}:\d{2}:\d{2}).\d+Z$/);
   const now = `${match[1]} ${match[2]}`
 
-  if (process.env.NODE_ENV === 'production')
+  if (getEnv('NODE_ENV', null) === 'production')
     console.log.call(console, `[${now}][${level}][${tag}]`, ...message);
   else
     console.log.call(console, `[${level}][${tag}]`, ...message);
 };
 
 module.exports = {
-  log:   log.bind(null, 'LOG'),
-  info:  log.bind(null, 'INF'),
+  getEnv,
+  log: log.bind(null, 'LOG'),
+  info: log.bind(null, 'INF'),
   error: log.bind(null, 'ERR'),
 };
