4a5339fa45a8f7ebf49b1a478465a493e5001f12
Nils Cox <nils@epita.fr>
2018-08-17 11:43:38 +0200

    api: add after hook to the game player routes

--- api/src/routes/game/game-players.js
+++ api/src/routes/game/game-players.js
@@ -2,6 +2,7 @@ const { BadRequestError } = require('../../errors');
 const { gameFormatter } = require('../../formatters');
 const { isPlayer, isInGame, isNotInGame } = require('../../permissions');
 const websockets = require('../../websockets');
+const { info } = require('../../utils');
 const findGame = require('./find-game');
 
 const router = require('../createRouter')();
@@ -22,9 +23,12 @@ router.post('/:id/join', {
     req => isNotInGame(req.player),
   ],
   format: format(),
-  after: async (req, game) => {
-    websockets.admin('GAME_JOIN', { game: await gameFormatter.admin(game) });
-    info('GAME', 'join', '#' + game.id, '(' + req.params.player.nick + ')');
+  after: async (req) => {
+    const { game } = req.params;
+    const { player } = req;
+
+    websockets.admin('GAME_JOIN', { game: await gameFormatter.admin(game), playerId: player.id });
+    info('GAME', 'join', '#' + game.id, '(' + player.nick + ')');
   },
 }, async (req, res, next) => {
   await req.params.game.join(req.player);
@@ -36,9 +40,12 @@ router.post('/:id/leave', {
     req => isPlayer(req.player),
     req => isInGame(req.player, req.params.id),
   ],
-  after: async (req, game) => {
-    websockets.admin('GAME_LEAVE', { game: await gameFormatter.admin(game) });
-    info('GAME', 'leave', '#' + game.id, '(' + req.params.player.nick + ')');
+  after: async (req) => {
+    const { game } = req.params;
+    const { player } = req;
+
+    websockets.admin('GAME_LEAVE', { game: await gameFormatter.admin(game), playerId: player.id });
+    info('GAME', 'leave', '#' + game.id, '(' + player.nick + ')');
   },
 }, async req => {
   await req.params.game.leave(req.player);
