059d8805422e3a0ce9e5004899727887992a10e7
Nils Cox <nils@epita.fr>
2018-08-22 18:46:53 +0200

    app: add auth text field

--- app/src/screens/auth/AuthScreen.js
+++ app/src/screens/auth/AuthScreen.js
@@ -1,34 +1,52 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import { Redirect } from 'react-router-native';
 
 import { login } from '../../services/auth-service';
 
 
+const styles = StyleSheet.create({
+  input: {
+     height: 40,
+     borderColor: 'gray',
+     borderBottomWidth: 1,
+  },
+});
+
 export default class AuthScreen extends React.Component {
 
   state = {
     player: null,
+    nick: '',
   };
 
-  async componentDidMount() {
-    const { res, json } = await login('nils');
+  async login(nick) {
+    const { res, json } = await login(nick);
 
-    if (res.status === 200)
+    if (res.status === 200) {
+      this.props.setPlayer(json);
       this.setState({ player: json });
+    }
     else
       console.log(json);
   }
 
   render() {
-    const { player } = this.state;
+    const { player, nick } = this.state;
 
     if (player)
       return <Redirect to={ player.gameId ? '/game/' + player.gameId : '/lobby' } />;
 
     return (
       <View>
-        <Text>auth</Text>
+        <TextInput
+          style={styles.input}
+          value={nick}
+          onChangeText={nick => this.setState({ nick })}
+        />
+        <TouchableOpacity onPress={() => this.login(nick)}>
+          <Text>LOG IN</Text>
+        </TouchableOpacity>
       </View>
     );
   }
