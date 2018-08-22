78082df50f4421ade91cd9885b4f38e1f8eff885
Nils Cox <nils@epita.fr>
2018-08-22 18:49:51 +0200

    app: handle websocket connection in the whole app

--- app/src/App.js
+++ app/src/App.js
@@ -3,7 +3,7 @@ import { Alert, Text } from 'react-native';
 import { NativeRouter, Switch, Route, Redirect } from 'react-router-native';
 
 import { fetchMe } from './services/player-service';
-import { createWebSocket } from './services/websocket-service';
+import { createWebSocket, emitter as websocket } from './services/websocket-service';
 
 import AuthScreen from './screens/auth/AuthScreen';
 import LobbyScreen from './screens/lobby/LobbyScreen';
@@ -19,6 +19,17 @@ export default class App extends React.Component {
     player: null,
   };
 
+  constructor(props) {
+    super(props);
+
+    websocket.on('player:update', (p) => {
+      const { player } = this.state;
+
+      if (player && player.id === p.id)
+        this.setState({ player: p });
+    });
+  }
+
   async componentDidMount() {
     await new Promise(r => setTimeout(r, 0));
 
@@ -32,13 +43,16 @@ export default class App extends React.Component {
     this.setState({ loading: false });
   }
 
+  setPlayer(player) {
+    this.setState({ player });
+    createWebSocket();
+  }
+
   render() {
     const { loading, player } = this.state;
     const getRouteAfterLoading = () => {
       if (!player)
         return '/auth';
-
-      createWebSocket();
 
       if (!player.gameId)
         return '/lobby';
@@ -53,7 +67,7 @@ export default class App extends React.Component {
       <NativeRouter>
         <Switch>
           <Route path="/" exact render={() => <Redirect to={getRouteAfterLoading()} />} />
-          <Route path="/auth" render={() => <AuthScreen setPlayer={p => this.setState(p)} />} />
+          <Route path="/auth" render={() => <AuthScreen setPlayer={player => this.setPlayer(player)} />} />
           <Route path="/lobby" component={LobbyScreen} />
           <Route path="/game/:id" render={props => <GameScreen player={player} {...props} />} />
           <Route render={() => <Text>404.</Text>} />
