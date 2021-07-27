605b6c29e4f5a395b3380f82cce3401f102333c2
Nils <nils@epita.fr>
2021-07-27 02:54:18 +0200

    add dotenv

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 import { createKnexConnection, createKnexSessionStore, main } from './infrastructure';
 import { bootstrapServer } from './infrastructure/web';
 import { WebsocketServer } from './infrastructure/web/websocket';
