6b39bd77d8f738d53220291b0c68d0ddae02b818
nilscox <nilscox.dev@gmail.com>
2023-06-19 19:58:27 +0200

    add authentication check

--- packages/server/src/server/http-server.ts
+++ packages/server/src/server/http-server.ts
@@ -1,9 +1,9 @@
-import { createServer, Server as NodeServer } from 'node:http';
+import { createServer, IncomingMessage, Server as NodeServer } from 'node:http';
 import { promisify } from 'node:util';
 
 import bodyParser from 'body-parser';
 import { Container } from 'ditox';
-import express from 'express';
+import express, { RequestHandler } from 'express';
 import session from 'express-session';
 import createMemoryStore from 'memorystore';
 import morgan from 'morgan';
@@ -11,6 +11,7 @@ import * as yup from 'yup';
 
 import { ConfigPort, LoggerPort } from 'src/adapters';
 import { TOKENS } from 'src/tokens';
+import { defined } from 'src/utils/defined';
 
 declare module 'express-session' {
   interface SessionData {
@@ -62,9 +63,17 @@ export class HttpServer {
   private configure() {
     this.app.set('trust proxy', 1);
 
-    this.app.use(this.loggerMiddleware);
     this.app.use(this.sessionMiddleware);
+    this.app.use(this.loggerMiddleware);
     this.app.use(bodyParser.json());
+
+    const authenticated: RequestHandler = (req, res, next) => {
+      if (!req.session.playerId) {
+        res.status(401).send('request must be authenticated');
+      } else {
+        next();
+      }
+    };
 
     this.app.get('/health-check', (req, res) => {
       res.status(200).end();
@@ -80,10 +89,11 @@ export class HttpServer {
       res.status(201).end();
     });
 
-    this.app.post('/game', async (req, res) => {
+    this.app.post('/game', authenticated, async (req, res) => {
+      const playerId = defined(req.session.playerId);
       const handler = this.container.resolve(TOKENS.commands.createGame);
 
-      await handler.execute({ creatorId: '' });
+      await handler.execute({ playerId });
       res.status(201).end();
     });
 
@@ -101,13 +111,9 @@ export class HttpServer {
       res.json(result);
     });
 
-    this.app.get('/me', async (req, res) => {
-      const playerId = req.session.playerId;
+    this.app.get('/me', authenticated, async (req, res) => {
+      const playerId = defined(req.session.playerId);
       const handler = this.container.resolve(TOKENS.queries.getPlayer);
-
-      if (!playerId) {
-        return res.status(404).end();
-      }
 
       const result = await handler.execute({ playerId });
       res.json(result);
@@ -117,9 +123,12 @@ export class HttpServer {
   private get loggerMiddleware() {
     const logger = this.logger;
 
+    type Req = IncomingMessage & { session: session.SessionData };
+
     const format: morgan.FormatFn = (tokens, req, res) => {
       return [
         tokens['remote-addr'](req, res),
+        (req as Req).session.playerId ?? 'unauthenticated',
         tokens.method(req, res),
         tokens.url(req, res),
         tokens.status(req, res),
