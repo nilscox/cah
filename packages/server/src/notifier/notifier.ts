4b8799dda4b3c8047db8252f3206bc764633878a
nilscox <nilscox.dev@gmail.com>
2023-06-20 17:31:19 +0200

    add events to the shared code

--- packages/server/src/notifier/notifier.ts
+++ packages/server/src/notifier/notifier.ts
@@ -1,5 +1,10 @@
+import { GameEvent } from '@cah/shared';
+
 import { EventPublisherPort, RealEventPublisherAdapter, RtcPort } from 'src/adapters';
+import { GameCreatedEvent } from 'src/commands/create-game/create-game';
 import { PlayerJoinedEvent } from 'src/commands/join-game/join-game';
+import { GameStartedEvent } from 'src/commands/start-game/start-game';
+import { isStarted } from 'src/entities';
 import { GameRepository, PlayerRepository } from 'src/persistence';
 import { PlayerConnectedEvent } from 'src/server/ws-server';
 
@@ -25,7 +30,20 @@ export class Notifier {
 
       const game = await this.gameRepository.findByIdOrFail(player.gameId);
 
-      await this.rtc.send(game.id, { type: 'player-connected', player });
+      await this.send(game.id, {
+        type: 'player-connected',
+        nick: player.nick,
+      });
+    });
+
+    publisher.register(GameCreatedEvent, async (event) => {
+      const game = await this.gameRepository.findByIdOrFail(event.entityId);
+
+      await this.send(game.id, {
+        type: 'game-created',
+        gameId: game.id,
+        code: game.code,
+      });
     });
 
     publisher.register(PlayerJoinedEvent, async (event) => {
@@ -38,7 +56,27 @@ export class Notifier {
 
       const game = await this.gameRepository.findByIdOrFail(player.gameId);
 
-      await this.rtc.send(game.id, { type: 'player-joined', player });
+      await this.send(game.id, {
+        type: 'player-joined',
+        nick: player.nick,
+        gameId: game.id,
+      });
     });
+
+    publisher.register(GameStartedEvent, async (event) => {
+      const game = await this.gameRepository.findByIdOrFail(event.entityId);
+      assert(isStarted(game));
+
+      await this.send(game.id, {
+        type: 'game-started',
+        gameId: game.id,
+        questionMasterId: game.questionMasterId,
+        questionId: game.questionId,
+      });
+    });
+  }
+
+  private async send(to: string, event: GameEvent) {
+    await this.rtc.send(to, event);
   }
 }
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -15,3 +15,39 @@ export type Player = {
   nick: string;
   gameId?: string;
 };
+
+export type PlayerConnectedEvent = {
+  type: 'player-connected';
+  nick: string;
+};
+
+export type PlayerDisconnectedEvent = {
+  type: 'player-connected';
+  nick: string;
+};
+
+export type GameCreatedEvent = {
+  type: 'game-created';
+  gameId: string;
+  code: string;
+};
+
+export type PlayerJoinedEvent = {
+  type: 'player-joined';
+  gameId: string;
+  nick: string;
+};
+
+export type GameStartedEvent = {
+  type: 'game-started';
+  gameId: string;
+  questionMasterId: string;
+  questionId: string;
+};
+
+export type GameEvent =
+  | PlayerConnectedEvent
+  | PlayerDisconnectedEvent
+  | GameCreatedEvent
+  | PlayerJoinedEvent
+  | GameStartedEvent;
