210a04f5970f635481df292374330ea9ac1e9a43
nils <nilscox.dev@gmail.com>
2026-09-23 16:32:44 +0200

    build docker images

--- apps/web/src/vite-env.d.ts
+++ apps/web/src/vite-env.d.ts
@@ -1,1 +1,11 @@
-/// <reference types="vite/client" />
+interface ViteTypeOptions {
+  strictImportMetaEnv: unknown;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [solid(), tailwindcss()],
+  build: {
+    sourcemap: true,
+  },
   server: {
     port: 8000,
     proxy: {
